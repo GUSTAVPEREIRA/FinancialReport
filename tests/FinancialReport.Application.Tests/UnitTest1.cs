3c5f10df721c72b02accdb93363e3b074dc3e58b
Pereira <gugupereira123@hotmail.com>
2024-08-05 22:24:43 -0300

    Fix some wrong projects that'd created with wrong type

---
+++ tests/FinancialReport.Application.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace FinancialReport.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/FinancialReport.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace FinancialReport.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
